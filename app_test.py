e9e5f340f0ff68ef9b01b99db33bfaa31a120934
ShresthShri <79257227+ShresthShri@users.noreply.github.com>
2024-10-21 16:19:10 +0100

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -2,8 +2,8 @@ from app import process_query
 
 
 def test_knows_about_dinosaurs():
-    assert process_query("dinosaurs") == "Dinosaurs ruled the Earth"
-    "200 million years ago"
+    din = "Dinosaurs ruled the Earth 200 million years ago"
+    assert process_query("dinosaurs") == din
 
 
 def test_does_not_know_about_asteroids():
