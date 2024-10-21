9e0752ab597115aec3432f98b8479d35b0c0a60a
qczeck <98347897+qczeck@users.noreply.github.com>
2024-10-21 15:36:47 +0100

    Update app.py

--- app.py
+++ app.py
@@ -10,12 +10,10 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/dinosaurs", methods=["GET"])
-def process_query(query):
-    if query == "dinosaurs":
-        return "Dinosaurs ruled the Earth 200 million years ago"
-    else:
-        return "Unknown"
+@app.route("/query", methods=["GET"])
+def query():
+    q = request.args.get('q')
+    process_query(q)
 
 
 @app.route("/submit", methods=["POST"])
@@ -28,3 +26,9 @@ def submit():
     else:
         return render_template("index.html", error="No Wikipedia page found"
                                "for this prompt. Please try again.")
+
+def process_query(query):
+    if query == "dinosaurs":
+        return "Dinosaurs ruled the Earth 200 million years ago"
+    else:
+        return "Unknown"
