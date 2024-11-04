51badba6a7067d39ed5b6337c4dbb89b11e1d5e5
ShresthShri <79257227+ShresthShri@users.noreply.github.com>
2024-11-04 20:03:58 +0000

    Final Commit

--- app_test.py
+++ app_test.py
@@ -15,13 +15,15 @@ def test_does_know_your_name():
 
 
 def test_finds_largest_number_out_of_three():
-    assert process_query("Which of the following numbers"
-                         " is the largest: 24, 47, 98?") == "98"
+    assert (
+        process_query(
+            "Which of the following numbers"
+            " is the largest: 24, 47, 98?") == "98")
 
 
 def test_does_sum_of_numbers_work():
-    assert process_query("What is 91 plus 30?") == '121'
+    assert process_query("What is 91 plus 30?") == "121"
 
 
 def test_does_product_of_numbers_work():
-    assert process_query("What is 53 multiplied by 78?") == '4134'
+    assert process_query("What is 53 multiplied by 78?") == "4134"
